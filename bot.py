f574e7d3b74535fe6976dd3088df5eca2b495f96
Hassan Farooq <1hassanfarooq@gmail.com>
2022-04-17 13:43:33 -0500

    Random tweet fetches, need to now respond

--- bot.py
+++ bot.py
@@ -1,28 +1,63 @@
-import os
+import json
 import openai
+import tweepy
+import time
+from random_word import RandomWords
 
-# Pull random tweet
-tweet = "This was posted on Twitter: \"i\’m not donating to them ppl bro stop asking me. somebody needa make a donation to ME\" Produce a response:"
+apiKeys = json.load(open("api_keys.json"))
 
-# Run openai completion on tweet
-openai.api_key =  open("api_key.txt", "r").read()
-out = openai.Completion.create(
-  engine="text-davinci-002",
-  prompt=tweet,
-  max_tokens=64,
-  frequency_penalty=0.5
+client = tweepy.Client(
+    consumer_key=apiKeys["consumer_key"], consumer_secret=apiKeys["consumer_secret"],
+    access_token=apiKeys["access_token"], access_token_secret=apiKeys["access_token_secret"]
 )
 
-# Ensure tweet isn't over max len (start thread instead of regen?)
-while(len(out.choices[0].text) > 280):
-    print("another")
+# auth = tweepy.OAuthHandler(apiKeys["consumer_key"], apiKeys["consumer_secret"])
+# auth.set_access_token(apiKeys["access_token"], apiKeys["access_token_secret"])
+# auth.secure = True
+# api = tweepy.API(auth)
+
+r = RandomWords()
+
+while(True):
+    # Get random tweet
+    randWord = r.get_random_word()
+
+    while(randWord is None):
+        randWord = r.get_random_word()
+
+    tweetData = client.search_recent_tweets(query=randWord, user_auth=True)
+
+    while(tweetData is None):
+        tweetData = client.search_recent_tweets(query=randWord, user_auth=True)
+    
+    tweetText = tweetData.data[0].text
+
+    print(tweetText)
+
+    # Run openai completion on tweet
+    openai.api_key = apiKeys["openai"]
     out = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=tweet,
+        prompt=tweetText,
         max_tokens=64,
         frequency_penalty=0.5
     )
 
-print(out.choices[0].text)
+    # Ensure tweet isn't over max len (start thread instead of regen?)
+    while(len(out.choices[0].text) > 280 or len(out.choices[0].text) < 1):
+        print("another")
+        out = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=tweetText,
+            max_tokens=64,
+            frequency_penalty=0.5
+        )
 
-# Send tweet
+    # Send tweet
+    response = client.create_tweet(
+        text=out.choices[0].text
+    )
+
+    print(f"https://twitter.com/user/status/{response.data['id']}")
+    time.sleep(60)
+
